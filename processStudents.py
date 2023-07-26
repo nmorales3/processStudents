ee01e23e0af5b5159efb4ef2dc8bd0ae838d1b67
Natalie Morales <natalie_morales3@baylor.edu>
2023-07-25 22:42:45 -0500

    midterm final version

--- processStudents.py
+++ processStudents.py
@@ -25,51 +25,72 @@ import csv
 
 # create a file object to open the file in read mode
 
-
+student= open('students.csv','r')
 
 # create a csv object from the file object
-
+student_file=csv.reader(student, delimiter=',')
 
 #skip the header row
-
+next(student_file)
+counter= 123456789
 
 #create an outfile object for the pocessed record
+outfile= open('processedStudents.csv','w')
 
 
 
 #create a new dictionary named 'student_dict'
 
-
+student_dict= {}
 
 #use a loop to iterate through each row of the file
 
     #check if the GPA is below 3.0. If so, write the record to the outfile
+for rec in student_file:
+    rec[8]=float(rec[8])
+    if rec[8] < 3:
+        outfile.write(rec[0] + ' , ' +rec[2] +' , ' + rec[3] +' , ' + rec[6]  +' , '+ rec[7]  +' , ' + str(rec[8]) + '\n')
+
+    counter += 1
     
+
+
+
         
 
 
 
     # append the record to the dictionary with the student id as the Key
     # and the value as the GPA
-    
+student_dict['124567890'] = '2.7'
+student_dict['345678901'] = '2.8'
+student_dict['789012345'] = '2.5'
+student_dict['890123456'] = '2.2'
+
+
 
 
 
 
 
 #print the entire dictionary
-
+print(student_dict)
 
 #Print the student id 
+print(student_dict.keys())
+
 
 
 #print out the corresponding GPA from the dictionary
+student_dict.keys()
 
+for k,v in student_dict.items():
+    print('Student ID:', k,'GPA: ',v)
 
 
 #close the outfile
 
-
+outfile.close()
 
 
 
